ce33f296a62ed9b366b9a7854d5b7fba302235e9
darrentran <darrenman456@outlook.com>
2017-11-18 21:48:15 -0700

    added splashscreen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,7 +4,7 @@ import { DataService } from '../../app/data.service';
 import { MangaList } from '../../models/manga.model';
 import { Observable } from 'rxjs/Observable';
 
-@IonicPage()
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -12,12 +12,24 @@ import { Observable } from 'rxjs/Observable';
 export class HomePage {
   chapter;
   mangaList: Observable<MangaList[]>;
+  tabBarElement: any;
+  splash = true;
 
   constructor(public navCtrl: NavController, public _data: DataService, public modalCtrl: ModalController) {
     // retrieve list of manga from site
+    this.tabBarElement = document.querySelector('.tabbar');
     this.mangaList = _data.getMangaList("mangareader.net")
     .map(data => data.slice(0, 100));
   }
+
+  ionViewDidLoad() {
+      this.tabBarElement.style.display = 'none';
+      setTimeout(() => {
+        this.splash = false;
+        this.tabBarElement.style.display = 'flex';
+      }, 4000);
+    }
+
 
   presentManga(name: string) {
     // Show Manga Detail Modal
